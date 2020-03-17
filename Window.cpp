05306cc9dd16bacef746ab5e3ad6ae409f7ccb18
mis046 <mis046@ucsd.edu>
2020-03-16 23:58:18 -0700

    added brightness

--- Window.cpp
+++ Window.cpp
@@ -106,8 +106,15 @@ bool Window::initializeProgram()
         cout << "Framebuffer complete";
     
     // If you want all rendering operations to have a visual impact again on the main window we need to make the default framebuffer active by binding to 0:
-    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+//    glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
+    unsigned int colorBuffers[2];
+    glGenTextures(2, colorBuffers);
+    
+    unsigned int attachments[2] = { GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1 };
+    glDrawBuffers(2, attachments);
+    
+    
     
 	return true;
 }
