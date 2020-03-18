94573d61c8450547063e02449929f366e6fb6aed
mis046 <mis046@ucsd.edu>
2020-03-17 19:23:28 -0700

    Update Window.cpp

--- Window.cpp
+++ Window.cpp
@@ -191,27 +191,6 @@ bool Window::initializeProgram()
     }
     glBindFramebuffer(GL_FRAMEBUFFER, 0);
     
-//    float quadVertices[] = { // vertex attributes for a quad that fills the entire screen in Normalized Device Coordinates.
-//        // positions   // texCoords
-//        -1.0f,  1.0f,  0.0f, 1.0f,
-//        -1.0f, -1.0f,  0.0f, 0.0f,
-//         1.0f, -1.0f,  1.0f, 0.0f,
-//
-//        -1.0f,  1.0f,  0.0f, 1.0f,
-//         1.0f, -1.0f,  1.0f, 0.0f,
-//         1.0f,  1.0f,  1.0f, 1.0f
-//    };
-//
-//    glGenVertexArrays(1, &quadVAO);
-//    glGenBuffers(1, &quadVBO);
-//    glBindVertexArray(quadVAO);
-//    glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
-//    glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
-//    glEnableVertexAttribArray(0);
-//    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
-//    glEnableVertexAttribArray(1);
-//    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
-//
     glUseProgram(blurShader);
     glUniform1i(glGetAttribLocation(blurShader, "image"), 0);
     
@@ -227,7 +206,7 @@ glm::vec3 Window::randV() {
 }
 
 int Window::randLife() {
-    return rand() % 1000+1000;
+    return rand() % 1000+500;
 }
 
 bool Window::initializeObjects()
